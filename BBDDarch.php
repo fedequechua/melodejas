4c9afd368fdb5105411fe121c4ead1823e2cc176
fedequechua <fddavicino@gmail.com>
2016-01-13 23:46:17 +0100

    Create BBDDarch.php

---
+++ BBDDarch.php
@@ -0,0 +1,14 @@
+// Esborrany: Cal revisar el llenguatge i l'arquitectura en si, que és només una proposta
+/**
+
+TABLE USERS: user_id, username, img, location, is_premium, trusty_points, untrusty_points, credit
+
+TABLE OBJECTS: obj_id, user_id, description, imgs, price, garantia, ratings,
+
+
+
+
+
+
+
+*/
